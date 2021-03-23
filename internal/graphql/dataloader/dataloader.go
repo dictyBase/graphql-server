aa1d40544c9c5027d3630a8681bd0753cd50bd24
wildlifehexagon <hartline.eric@gmail.com>
2021-03-23 17:29:45 -0500

    feat: add initial dataloader logic with middleware

--- internal/graphql/dataloader/dataloader.go
+++ internal/graphql/dataloader/dataloader.go
@@ -1,14 +1,10 @@
 package dataloader
 
-//go:generate go run github.com/vektah/dataloaden StrainLoader string *github.com/dictyBase/graphql-server/internal/graphql/models.Strain
-
 import (
 	"context"
 	"time"
 
-	pb "github.com/dictyBase/go-genproto/dictybaseapis/stock"
 	"github.com/dictyBase/graphql-server/internal/graphql/models"
-	"github.com/dictyBase/graphql-server/internal/graphql/resolver/stock"
 	"github.com/dictyBase/graphql-server/internal/registry"
 )
 
@@ -26,42 +22,13 @@ func newLoaders(ctx context.Context, nr registry.Registry) *Loaders {
 	}
 }
 
-// Retriever retrieves dataloaders from the request context.
-type Retriever interface {
-	Retrieve(context.Context) *Loaders
-}
-
-type retriever struct {
-	key contextKey
-}
-
-func (r *retriever) Retrieve(ctx context.Context) *Loaders {
-	return ctx.Value(r.key).(*Loaders)
-}
-
-// NewRetriever instantiates a new implementation of Retriever.
-func NewRetriever() Retriever {
-	return &retriever{key: key}
-}
-
 func newStrainById(ctx context.Context, nr registry.Registry) *StrainLoader {
 	return NewStrainLoader(StrainLoaderConfig{
 		MaxBatch: 100,
-		Wait:     100 * time.Millisecond,
+		Wait:     1 * time.Millisecond,
 		Fetch: func(ids []string) ([]*models.Strain, []error) {
-			strains := make([]*models.Strain, 0)
-			sl, err := nr.GetStockClient(registry.STOCK).ListStrainsByIds(
-				context.Background(),
-				&pb.StockIdList{Id: ids},
-			)
-			if err != nil {
-				return strains, []error{err}
-			}
-			for _, sd := range sl.Data {
-				strains = append(
-					strains,
-					stock.ConvertToStrainModel(sd.Id, sd.Attributes))
-			}
+			strains := []*models.Strain{}
+			// add logic here...
 			return strains, nil
 		},
 	})
