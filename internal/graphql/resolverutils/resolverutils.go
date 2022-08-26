639363932791225800e460695d8fa0751b9b6f3e
Siddhartha Basu <biosidd@gmail.com>
2022-08-26 12:27:50 -0500

    refactor: add resolverutils package to collect all reusable functions

---
+++ internal/graphql/resolverutils/resolverutils.go
@@ -0,0 +1,112 @@
+package resolverutils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/dictyBase/graphql-server/internal/graphql/models"
+	"github.com/dictyBase/graphql-server/internal/registry"
+)
+
+func GetCursor(c *int) int64 {
+	if c == nil {
+		return int64(0)
+	}
+	return int64(*c)
+}
+
+func GetLimit(l *int) int64 {
+	if l == nil {
+		return int64(10)
+	}
+	return int64(*l)
+}
+
+func GetFilter(f *string) string {
+	if f == nil {
+		return ""
+	}
+	return *f
+}
+
+func strainTypeQuery(filter *models.StrainListFilter) (string, error) {
+	switch filter.StrainType {
+	case models.StrainTypeEnumAll:
+		return fmt.Sprintf(
+			"ontology==%s;tag==%s,tag==%s,tag==%s",
+			registry.DictyStrainPropOntology,
+			registry.GeneralStrainTag,
+			registry.GwdiStrainTag,
+			registry.BacterialStrainTag,
+		), nil
+	case models.StrainTypeEnumBacterial:
+		return fmt.Sprintf(
+			"ontology==%s;tag==%s",
+			registry.DictyStrainPropOntology,
+			registry.BacterialStrainTag,
+		), nil
+	case models.StrainTypeEnumRegular:
+		return fmt.Sprintf(
+			"ontology==%s;tag==%s",
+			registry.DictyStrainPropOntology,
+			registry.GeneralStrainTag,
+		), nil
+	case models.StrainTypeEnumGwdi:
+		return fmt.Sprintf(
+			"ontology==%s;tag==%s",
+			registry.DictyStrainPropOntology,
+			registry.GwdiStrainTag,
+		), nil
+	}
+
+	return "", fmt.Errorf("invalid strain type %s", filter.StrainType.String())
+}
+
+func strainFieldsQuery(filter *models.StrainListFilter) string {
+	var query strings.Builder
+	if len(*filter.Label) > 0 {
+		query.WriteString(fmt.Sprintf("label==%s", *filter.Label))
+	}
+	if len(*filter.Summary) > 0 {
+		if query.Len() > 0 {
+			query.WriteString(fmt.Sprintf(";summary==%s", *filter.Summary))
+		} else {
+			query.WriteString(fmt.Sprintf("summary==%s", *filter.Summary))
+		}
+	}
+
+	return query.String()
+}
+
+func StrainFilterToQuery(filter *models.StrainListFilter) (string, error) {
+	var query strings.Builder
+	query.WriteString(strainFieldsQuery(filter))
+	typeQuery, err := strainTypeQuery(filter)
+	if err != nil {
+		return query.String(), err
+	}
+	if query.Len() > 0 {
+		query.WriteString(fmt.Sprintf(";%s", typeQuery))
+	} else {
+		query.WriteString(typeQuery)
+	}
+
+	return query.String(), nil
+}
+
+func GetOntology(onto string) string {
+	var oname string
+	switch onto {
+	case "phenotype":
+		oname = registry.PhenoOntology
+	case "characteristic":
+		oname = registry.StrainCharOnto
+	case "strain_inventory":
+		oname = registry.StrainInvOnto
+	case "plasmid_inventory":
+		oname = registry.PlasmidInvOnto
+	default:
+		oname = "invalid ontology"
+	}
+	return oname
+}
