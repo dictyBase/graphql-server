06552b44d245056d8152096e734b33bb38dfc47e
Siddhartha Basu <biosidd@gmail.com>
2022-09-20 13:17:54 -0500

    refactor: switch to using the autogenerated model for publication

--- internal/graphql/fetch/publication.go
+++ internal/graphql/fetch/publication.go
@@ -9,9 +9,8 @@ import (
 	"time"
 
 	"github.com/Jeffail/gabs/v2"
-	"github.com/dictyBase/aphgrpc"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/publication"
-	"github.com/sirupsen/logrus"
+	"github.com/dictyBase/graphql-server/internal/graphql/models"
 )
 
 type PubJsonAPI struct {
@@ -55,118 +54,111 @@ type Author struct {
 	Initials  string `json:"initials"`
 }
 
-func FetchPublication(ctx context.Context, endpoint, id string) (*pb.Publication, error) {
-	logger := *logrus.New()
-	url := endpoint + "/" + id
-	res, err := GetResp(ctx, url)
+func FetchPublication(
+	ctx context.Context,
+	endpoint, id string,
+) (*models.Publication, error) {
+	res, err := GetResp(ctx, fmt.Sprintf("%s/%s", endpoint, id))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error in getting response %s", err)
 	}
-	decoder := json.NewDecoder(res.Body)
-	var pub PubJsonAPI
-	err = decoder.Decode(&pub)
-	if err != nil {
+	pub := &PubJsonAPI{}
+	if err := json.NewDecoder(res.Body).Decode(pub); err != nil {
 		return nil, fmt.Errorf("error decoding json %s", err)
 	}
-	attr := pub.Data.Attributes
 	// convert pub_date to expected time.Time format
-	pd, err := time.Parse("2006-01-02", attr.PublishedDate)
+	pd, err := time.Parse("2006-01-02", pub.Data.Attributes.PublishedDate)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse published date %s", err)
 	}
-	p := &pb.Publication{
-		Data: &pb.Publication_Data{
-			Type: "publication",
-			Id:   id,
-			Attributes: &pb.PublicationAttributes{
-				Doi:      attr.Doi,
-				Title:    attr.Title,
-				Abstract: attr.Abstract,
-				Journal:  attr.Journal,
-				PubDate:  aphgrpc.TimestampProto(pd),
-				Pages:    attr.Page,
-				Issn:     attr.Issn,
-				PubType:  attr.PubType,
-				Source:   attr.Source,
-				Issue:    attr.Issue,
-				Volume:   attr.Volume,
-				Status:   attr.Status,
-			},
-		},
+	mpub := &models.Publication{
+		ID:       id,
+		Title:    pub.Data.Attributes.Title,
+		Abstract: pub.Data.Attributes.Abstract,
+		Journal:  pub.Data.Attributes.Journal,
+		Source:   pub.Data.Attributes.Source,
+		PubType:  pub.Data.Attributes.PubType,
+		Doi:      &pub.Data.Attributes.Doi,
+		Issue:    &pub.Data.Attributes.Issue,
+		Status:   &pub.Data.Attributes.Status,
+		Volume:   &pub.Data.Attributes.Volume,
+		Pages:    &pub.Data.Attributes.Page,
+		Issn:     &pub.Data.Attributes.Issn,
+		PubDate:  &pd,
 	}
-	var authors []*pb.Author
-	for i, a := range attr.Authors {
-		authors = append(authors, &pb.Author{
+
+	for i, a := range pub.Data.Attributes.Authors {
+		mpub.Authors = append(mpub.Authors, &pb.Author{
 			FirstName: a.FirstName,
 			LastName:  a.LastName,
 			Rank:      int64(i),
 			Initials:  a.Initials,
 		})
 	}
-	p.Data.Attributes.Authors = authors
-	logger.Debugf("successfully found publication with ID %s", pub.Data.ID)
-	return p, nil
+	return mpub, nil
 }
 
-func FetchDOI(ctx context.Context, doi string) (*pb.Publication, error) {
-	pub := &pb.Publication{}
+func FetchDOI(ctx context.Context, doi string) (*models.Publication, error) {
 	res, err := getDOIResp(ctx, doi)
 	if err != nil {
-		return pub, err
+		return nil, fmt.Errorf("error in getting response for doi %s", err)
 	}
 	defer res.Body.Close()
-	decoder := json.NewDecoder(res.Body)
-	j, err := gabs.ParseJSONDecoder(decoder)
+	jstruct, err := gabs.ParseJSONDecoder(json.NewDecoder(res.Body))
 	if err != nil {
-		return pub, err
+		return nil, fmt.Errorf("error in decoding json %s", err)
 	}
-	authors := getDOIAuthors(j.Search("author").Children())
-	d := j.Search("created", "date-time").Data().(string)
-	pd, err := time.Parse(time.RFC3339, d)
+	authors := getDOIAuthors(jstruct.Search("author").Children())
+	dateStr, ok := jstruct.Search("created", "date-time").Data().(string)
+	if !ok {
+		return nil, fmt.Errorf(
+			"error in converting date time to string %s",
+			err,
+		)
+	}
+	pd, err := time.Parse(time.RFC3339, dateStr)
 	if err != nil {
 		return nil, fmt.Errorf("could not parse published date %s", err)
 	}
-	p := &pb.Publication{
-		Data: &pb.Publication_Data{
-			Id: "", // doi do not have pubmed IDs listed
-			Attributes: &pb.PublicationAttributes{
-				Doi:      doi,
-				Title:    verifyStringProperty(j, "title"),
-				Abstract: verifyStringProperty(j, "abstract"),
-				Journal:  verifyStringProperty(j, "container-title-short"),
-				PubDate:  aphgrpc.TimestampProto(pd),
-				Volume:   verifyStringProperty(j, "volume"),
-				Pages:    verifyStringProperty(j, "page"),
-				Issn:     verifyArrayProperty(j, "ISSN"),
-				PubType:  verifyStringProperty(j, "type"),
-				Source:   verifyStringProperty(j, "source"),
-				Issue:    verifyStringProperty(j, "issue"),
-				Status:   verifyStringProperty(j, "subtype"),
-				Authors:  authors,
-			},
-		},
+	mpub := &models.Publication{
+		ID:       "",
+		Doi:      &doi,
+		Title:    verifyStringProperty(jstruct, "title"),
+		Abstract: verifyStringProperty(jstruct, "abstract"),
+		Journal: verifyStringProperty(
+			jstruct,
+			"container-title-short",
+		),
+		Source:  verifyStringProperty(jstruct, "source"),
+		PubType: verifyStringProperty(jstruct, "type"),
+		Issue:   verifyStringPointerProperty(jstruct, "issue"),
+		Status:  verifyStringPointerProperty(jstruct, "subtype"),
+		Volume:  verifyStringPointerProperty(jstruct, "volume"),
+		Pages:   verifyStringPointerProperty(jstruct, "page"),
+		Issn:    verifyStringPointerProperty(jstruct, "ISSN"),
+		Authors: authors,
+		PubDate: &pd,
 	}
-	return p, nil
+	return mpub, nil
 }
 
 // getDOIResp makes HTTP request with necessary
 // headers for DOI and returns the response
 func getDOIResp(ctx context.Context, doi string) (*http.Response, error) {
-	r := &http.Response{}
 	url, err := url.Parse(doi)
 	if err != nil {
-		return r, err
+		return nil, fmt.Errorf("error in parsing doi url %s", err)
 	}
-	req := &http.Request{
-		Method: "GET",
-		URL:    url,
-		Header: map[string][]string{
-			"Accept": {"application/vnd.citationstyles.csl+json"},
-		},
-	}
-	res, err := http.DefaultClient.Do(req)
+	res, err := http.DefaultClient.Do(
+		&http.Request{
+			Method: "GET",
+			URL:    url,
+			Header: map[string][]string{
+				"Accept": {"application/vnd.citationstyles.csl+json"},
+			},
+		})
 	if err != nil {
-		return res, err
+		return res, fmt.Errorf("error in doi http request %s", err)
 	}
 	return res, nil
 }
@@ -189,20 +181,36 @@ func getDOIAuthors(authors []*gabs.Container) []*pb.Author {
 	return a
 }
 
+func verifyStringPointerProperty(jstruct *gabs.Container, val string) *string {
+	emptyStr := ""
+	if !jstruct.Exists(val) {
+		return &emptyStr
+	}
+	str, ok := jstruct.Search(val).Data().(string)
+	if !ok {
+		return &emptyStr
+	}
+	return &str
+}
+
 // verifyStringProperty checks if a property exists in the JSON and returns the
 // value if true
-func verifyStringProperty(j *gabs.Container, val string) string {
-	if j.Exists(val) {
-		return j.Search(val).Data().(string)
+func verifyStringProperty(jstruct *gabs.Container, val string) string {
+	if !jstruct.Exists(val) {
+		return ""
 	}
-	return ""
+	str, ok := jstruct.Search(val).Data().(string)
+	if !ok {
+		return ""
+	}
+	return str
 }
 
 // verifyArrayProperty checks if a property exists in the JSON and then returns
 // its first child as a string
-func verifyArrayProperty(j *gabs.Container, val string) string {
-	if j.Exists(val) {
-		return j.Search(val).Children()[0].Data().(string)
+func verifyArrayProperty(jstruct *gabs.Container, val string) string {
+	if jstruct.Exists(val) {
+		return jstruct.Search(val).Children()[0].Data().(string)
 	}
 	return ""
 }
--- internal/graphql/resolver/publication/publication.go
+++ internal/graphql/resolver/publication/publication.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"time"
 
-	"github.com/dictyBase/aphgrpc"
+	"github.com/dictyBase/graphql-server/internal/graphql/models"
+
 	"github.com/dictyBase/go-genproto/dictybaseapis/publication"
 	"github.com/sirupsen/logrus"
 )
@@ -13,46 +14,93 @@ type PublicationResolver struct {
 	Logger *logrus.Entry
 }
 
-func (r *PublicationResolver) ID(ctx context.Context, obj *publication.Publication) (string, error) {
-	return obj.Data.Id, nil
+func (r *PublicationResolver) Doi(
+	ctx context.Context,
+	obj *models.Publication,
+) (*string, error) {
+	return obj.Doi, nil
 }
-func (r *PublicationResolver) Doi(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Doi, nil
+
+func (r *PublicationResolver) Title(
+	ctx context.Context,
+	obj *models.Publication,
+) (string, error) {
+	return obj.Title, nil
 }
-func (r *PublicationResolver) Title(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Title, nil
+
+func (r *PublicationResolver) Abstract(
+	ctx context.Context,
+	obj *models.Publication,
+) (string, error) {
+	return obj.Abstract, nil
 }
-func (r *PublicationResolver) Abstract(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Abstract, nil
+
+func (r *PublicationResolver) Journal(
+	ctx context.Context,
+	obj *models.Publication,
+) (string, error) {
+	return obj.Journal, nil
 }
-func (r *PublicationResolver) Journal(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Journal, nil
+
+func (r *PublicationResolver) PubDate(
+	ctx context.Context,
+	obj *models.Publication,
+) (*time.Time, error) {
+	return obj.PubDate, nil
 }
-func (r *PublicationResolver) PubDate(ctx context.Context, obj *publication.Publication) (*time.Time, error) {
-	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.PubDate)
-	return &time, nil
+
+func (r *PublicationResolver) Volume(
+	ctx context.Context,
+	obj *models.Publication,
+) (*string, error) {
+	return obj.Volume, nil
 }
-func (r *PublicationResolver) Volume(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Volume, nil
+
+func (r *PublicationResolver) Pages(
+	ctx context.Context,
+	obj *models.Publication,
+) (*string, error) {
+	return obj.Pages, nil
 }
-func (r *PublicationResolver) Pages(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Pages, nil
+
+func (r *PublicationResolver) Issn(
+	ctx context.Context,
+	obj *models.Publication,
+) (*string, error) {
+	return obj.Issn, nil
 }
-func (r *PublicationResolver) Issn(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Issn, nil
+
+func (r *PublicationResolver) PubType(
+	ctx context.Context,
+	obj *models.Publication,
+) (string, error) {
+	return obj.PubType, nil
 }
-func (r *PublicationResolver) PubType(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.PubType, nil
+
+func (r *PublicationResolver) Source(
+	ctx context.Context,
+	obj *models.Publication,
+) (string, error) {
+	return obj.Source, nil
 }
-func (r *PublicationResolver) Source(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Source, nil
+
+func (r *PublicationResolver) Issue(
+	ctx context.Context,
+	obj *models.Publication,
+) (*string, error) {
+	return obj.Issue, nil
 }
-func (r *PublicationResolver) Issue(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Issue, nil
+
+func (r *PublicationResolver) Status(
+	ctx context.Context,
+	obj *models.Publication,
+) (*string, error) {
+	return obj.Status, nil
 }
-func (r *PublicationResolver) Status(ctx context.Context, obj *publication.Publication) (*string, error) {
-	return &obj.Data.Attributes.Status, nil
-}
-func (r *PublicationResolver) Authors(ctx context.Context, obj *publication.Publication) ([]*publication.Author, error) {
-	return obj.Data.Attributes.Authors, nil
+
+func (r *PublicationResolver) Authors(
+	ctx context.Context,
+	obj *models.Publication,
+) ([]*publication.Author, error) {
+	return obj.Authors, nil
 }
