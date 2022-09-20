f8ade26119a59af30fb7e1d9f54cdda9f1368563
Siddhartha Basu <biosidd@gmail.com>
2022-09-20 17:56:10 -0500

    test: fix doi fetch test by using the correct publication model

--- internal/graphql/fetch/publication_test.go
+++ internal/graphql/fetch/publication_test.go
@@ -45,21 +45,25 @@ func TestFetchDOI(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(doiHandler))
 	defer ts.Close()
 	assert := assert.New(t)
-	g, err := FetchDOI(context.Background(), ts.URL)
+	gpub, err := FetchDOI(context.Background(), ts.URL)
 	assert.NoError(err, "should not have error when getting http response")
 	// expected empty values
-	assert.Empty(g.Data.Id, "should have empty string for ID")
-	assert.Empty(g.Data.Attributes.Issn, "should have empty string for ISSN")
-	assert.Empty(g.Data.Attributes.Journal, "should have empty string for journal")
-	assert.Empty(g.Data.Attributes.Volume, "should have empty string for volume")
-	assert.Empty(g.Data.Attributes.Pages, "should have empty string for pages")
-	assert.Empty(g.Data.Attributes.Issue, "should have empty string for issue")
+	assert.Empty(gpub.ID, "should have empty string for ID")
+	assert.Empty(gpub.Issn, "should have empty string for ISSN")
+	assert.Empty(gpub.Journal, "should have empty string for journal")
+	assert.Empty(gpub.Volume, "should have empty string for volume")
+	assert.Empty(gpub.Pages, "should have empty string for pages")
+	assert.Empty(gpub.Issue, "should have empty string for issue")
 	// verify authors length
-	assert.Len(g.Data.Attributes.Authors, 9, "should match number of authors")
+	assert.Len(gpub.Authors, 9, "should match number of authors")
 	// expected string values
-	assert.Equal(g.Data.Attributes.Status, "preprint", "should match status")
-	assert.Equal(g.Data.Attributes.Source, "Crossref", "should match source")
-	assert.Equal(g.Data.Attributes.Doi, ts.URL, "should match doi")
-	assert.Equal(g.Data.Attributes.PubType, "posted-content", "should match pub type")
-	assert.Equal(g.Data.Attributes.Abstract, "<jats:title>Abstract</jats:title><jats:p>Genomes can be sequenced with relative ease, but ascribing gene function remains a major challenge. Genetically tractable model systems are crucial to meet this challenge. One powerful model is the social amoeba<jats:italic>Dictyostelium discoideum</jats:italic>, a eukaryotic microbe widely used to study diverse questions in cell, developmental and evolutionary biology. However, its utility is hampered by the inefficiency with which sequence, transcriptome or proteome variation can be linked to phenotype. To address this, we have developed methods (REMI-seq) to (1) generate a near genome-wide resource of individual mutants (2) allow large-scale parallel phenotyping. We demonstrate that integrating these resources allows novel regulators of cell migration, phagocytosis and macropinocytosis to be rapidly identified. Therefore, these methods and resources provide a step change for high throughput gene discovery in a key model system, and the study of genes affecting traits associated with higher eukaryotes.</jats:p>", "should match doi")
+	assert.Equal(*gpub.Status, "preprint", "should match status")
+	assert.Equal(gpub.Source, "Crossref", "should match source")
+	assert.Equal(*gpub.Doi, ts.URL, "should match doi")
+	assert.Equal(gpub.PubType, "posted-content", "should match pub type")
+	assert.Equal(
+		gpub.Abstract,
+		"<jats:title>Abstract</jats:title><jats:p>Genomes can be sequenced with relative ease, but ascribing gene function remains a major challenge. Genetically tractable model systems are crucial to meet this challenge. One powerful model is the social amoeba<jats:italic>Dictyostelium discoideum</jats:italic>, a eukaryotic microbe widely used to study diverse questions in cell, developmental and evolutionary biology. However, its utility is hampered by the inefficiency with which sequence, transcriptome or proteome variation can be linked to phenotype. To address this, we have developed methods (REMI-seq) to (1) generate a near genome-wide resource of individual mutants (2) allow large-scale parallel phenotyping. We demonstrate that integrating these resources allows novel regulators of cell migration, phagocytosis and macropinocytosis to be rapidly identified. Therefore, these methods and resources provide a step change for high throughput gene discovery in a key model system, and the study of genes affecting traits associated with higher eukaryotes.</jats:p>",
+		"should match doi",
+	)
 }
