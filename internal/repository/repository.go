0b2dd3c5cf3ece6ba03904ff2770ecda36b76d00
Siddhartha Basu <biosidd@gmail.com>
2023-11-09 14:15:42 -0600

    feat(repository.go): add SetWithTTL method to Repository interface
    The SetWithTTL method is added to the Repository interface to allow
    setting a value with a specified Time To Live (TTL). This feature
    provides the ability to have keys in the repository that automatically
    expire after a certain duration, which can be useful for caching or
    other temporary data storage needs.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,11 @@
 package repository
 
+import "time"
+
 type Repository interface {
 	Get(string) (string, error)
 	Set(string, string) error
+	SetWithTTL(string, string, time.Duration) error
 	Exists(string) (bool, error)
 	HGet(string, string) (string, error)
 	HSet(string, string, string) error
