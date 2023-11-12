5c64611a65fe781b056a50b4b270198aac96d20f
Siddhartha Basu <biosidd@gmail.com>
2023-11-11 18:07:44 -0600

    feat(user.go): update Roles function to use new authentication client and handle role data differently
    The UserResolver's Client type has been changed to
    *authentication.LogtoClient to align with the new authentication system.
    The function parameters have been broken down into multiple lines to
    improve readability and maintainability of the code. The Roles function
    has been updated to use the new authentication client and handle role
    data differently, which is necessary due to changes in the way role data
    is now being received from the new authentication system.

--- internal/graphql/resolver/user/user.go
+++ internal/graphql/resolver/user/user.go
@@ -6,90 +6,159 @@ import (
 	"time"
 
 	"github.com/dictyBase/aphgrpc"
-	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"github.com/dictyBase/graphql-server/internal/authentication"
 	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
 	"github.com/sirupsen/logrus"
 )
 
 type UserResolver struct {
-	Client pb.UserServiceClient
+	Client *authentication.LogtoClient
 	Logger *logrus.Entry
 }
 
-func (r *UserResolver) ID(ctx context.Context, obj *pb.User) (string, error) {
+func (urs *UserResolver) ID(ctx context.Context, obj *pb.User) (string, error) {
 	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
-func (r *UserResolver) FirstName(ctx context.Context, obj *pb.User) (string, error) {
+
+func (urs *UserResolver) FirstName(
+	ctx context.Context,
+	obj *pb.User,
+) (string, error) {
 	return obj.Data.Attributes.FirstName, nil
 }
-func (r *UserResolver) LastName(ctx context.Context, obj *pb.User) (string, error) {
+
+func (urs *UserResolver) LastName(
+	ctx context.Context,
+	obj *pb.User,
+) (string, error) {
 	return obj.Data.Attributes.LastName, nil
 }
-func (r *UserResolver) Email(ctx context.Context, obj *pb.User) (string, error) {
+
+func (urs *UserResolver) Email(
+	ctx context.Context,
+	obj *pb.User,
+) (string, error) {
 	return obj.Data.Attributes.Email, nil
 }
-func (r *UserResolver) Organization(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) Organization(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.Organization, nil
 }
-func (r *UserResolver) GroupName(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) GroupName(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.GroupName, nil
 }
-func (r *UserResolver) FirstAddress(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) FirstAddress(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.FirstAddress, nil
 }
-func (r *UserResolver) SecondAddress(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) SecondAddress(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.SecondAddress, nil
 }
-func (r *UserResolver) City(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) City(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.City, nil
 }
-func (r *UserResolver) State(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) State(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.State, nil
 }
-func (r *UserResolver) Zipcode(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) Zipcode(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.Zipcode, nil
 }
-func (r *UserResolver) Country(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) Country(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.Country, nil
 }
-func (r *UserResolver) Phone(ctx context.Context, obj *pb.User) (*string, error) {
+
+func (urs *UserResolver) Phone(
+	ctx context.Context,
+	obj *pb.User,
+) (*string, error) {
 	return &obj.Data.Attributes.Phone, nil
 }
-func (r *UserResolver) IsActive(ctx context.Context, obj *pb.User) (bool, error) {
+
+func (urs *UserResolver) IsActive(
+	ctx context.Context,
+	obj *pb.User,
+) (bool, error) {
 	return obj.Data.Attributes.IsActive, nil
 }
-func (r *UserResolver) CreatedAt(ctx context.Context, obj *pb.User) (*time.Time, error) {
+
+func (urs *UserResolver) CreatedAt(
+	ctx context.Context,
+	obj *pb.User,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt)
 	return &time, nil
 }
-func (r *UserResolver) UpdatedAt(ctx context.Context, obj *pb.User) (*time.Time, error) {
+
+func (urs *UserResolver) UpdatedAt(
+	ctx context.Context,
+	obj *pb.User,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt)
 	return &time, nil
 }
-func (r *UserResolver) Roles(ctx context.Context, obj *pb.User) ([]*pb.Role, error) {
+
+func (urs *UserResolver) Roles(
+	ctx context.Context,
+	obj *pb.User,
+) ([]*pb.Role, error) {
 	roles := []*pb.Role{}
-	rr, err := r.Client.GetRelatedRoles(ctx, &jsonapi.RelationshipRequest{Id: obj.Data.Id})
+	resp, err := urs.Client.Roles(strconv.FormatInt(obj.Data.Id, 10))
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
-		r.Logger.Error(err)
+		urs.Logger.Error(err)
 		return roles, err
 	}
-	for _, n := range rr.Data {
+	for _, rdata := range resp {
+		roleId, _ := strconv.ParseInt(rdata.Id, 10, 64)
 		item := &pb.Role{
 			Data: &pb.RoleData{
 				Type: "role",
-				Id:   n.Id,
+				Id:   roleId,
 				Attributes: &pb.RoleAttributes{
-					Role:        n.Attributes.Role,
-					Description: n.Attributes.Description,
-					CreatedAt:   n.Attributes.CreatedAt,
-					UpdatedAt:   n.Attributes.UpdatedAt,
+					Role:        rdata.Name,
+					Description: rdata.Description,
+					CreatedAt:   obj.Data.Attributes.CreatedAt,
+					UpdatedAt:   obj.Data.Attributes.UpdatedAt,
 				},
 			},
 		}
 		roles = append(roles, item)
 	}
-	r.Logger.Infof("successfully retrieved list of %d roles for user ID %d", len(roles), obj.Data.Id)
+	urs.Logger.Infof(
+		"successfully retrieved list of %d roles for user ID %d",
+		len(roles),
+		obj.Data.Id,
+	)
 	return roles, nil
 }
