276ffe6289f97b90896204cf3a46cd8ac498dab0
Siddhartha Basu <biosidd@gmail.com>
2023-11-13 08:38:23 -0600

    refactor(content.go): change ContentResolver receiver name from 'r' to 'rcs' for better readability
    The receiver name 'rcs' is more descriptive and improves code
    readability. The change from user.UserServiceClient to
    authentication.LogtoClient is done to enhance the authentication process
    by using a more specialized client for handling user authentication.

--- internal/graphql/resolver/content/content.go
+++ internal/graphql/resolver/content/content.go
@@ -10,59 +10,60 @@ import (
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/content"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
 	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
+	"github.com/dictyBase/graphql-server/internal/authentication"
 	"github.com/sirupsen/logrus"
 )
 
 type ContentResolver struct {
 	Client     pb.ContentServiceClient
-	UserClient user.UserServiceClient
+	UserClient *authentication.LogtoClient
 	Logger     *logrus.Entry
 }
 
-func (r *ContentResolver) ID(ctx context.Context, obj *pb.Content) (string, error) {
+func (rcs *ContentResolver) ID(ctx context.Context, obj *pb.Content) (string, error) {
 	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
-func (r *ContentResolver) Name(ctx context.Context, obj *pb.Content) (string, error) {
+func (rcs *ContentResolver) Name(ctx context.Context, obj *pb.Content) (string, error) {
 	return obj.Data.Attributes.Name, nil
 }
-func (r *ContentResolver) Slug(ctx context.Context, obj *pb.Content) (string, error) {
+func (rcs *ContentResolver) Slug(ctx context.Context, obj *pb.Content) (string, error) {
 	return obj.Data.Attributes.Slug, nil
 }
-func (r *ContentResolver) CreatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
+func (rcs *ContentResolver) CreatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
 	user := user.User{}
 	id := obj.Data.Attributes.CreatedBy
-	g, err := r.UserClient.GetUser(ctx, &jsonapi.GetRequest{Id: id})
+	g, err := rcs.UserClient.GetUser(ctx, &jsonapi.GetRequest{Id: id})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
-		r.Logger.Error(err)
+		rcs.Logger.Error(err)
 		return &user, err
 	}
-	r.Logger.Debugf("successfully found user with id %d", id)
+	rcs.Logger.Debugf("successfully found user with id %d", id)
 	return g, nil
 }
-func (r *ContentResolver) UpdatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
+func (rcs *ContentResolver) UpdatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
 	user := user.User{}
 	id := obj.Data.Attributes.UpdatedBy
-	g, err := r.UserClient.GetUser(ctx, &jsonapi.GetRequest{Id: id})
+	g, err := rcs.UserClient.GetUser(ctx, &jsonapi.GetRequest{Id: id})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
-		r.Logger.Error(err)
+		rcs.Logger.Error(err)
 		return &user, err
 	}
-	r.Logger.Debugf("successfully found user with id %d", id)
+	rcs.Logger.Debugf("successfully found user with id %d", id)
 	return g, nil
 }
-func (r *ContentResolver) CreatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
+func (rcs *ContentResolver) CreatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt)
 	return &time, nil
 }
-func (r *ContentResolver) UpdatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
+func (rcs *ContentResolver) UpdatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt)
 	return &time, nil
 }
-func (r *ContentResolver) Content(ctx context.Context, obj *pb.Content) (string, error) {
+func (rcs *ContentResolver) Content(ctx context.Context, obj *pb.Content) (string, error) {
 	return obj.Data.Attributes.Content, nil
 }
-func (r *ContentResolver) Namespace(ctx context.Context, obj *pb.Content) (string, error) {
+func (rcs *ContentResolver) Namespace(ctx context.Context, obj *pb.Content) (string, error) {
 	return obj.Data.Attributes.Namespace, nil
 }
