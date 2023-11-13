cacdc54a6f2ad640cc725fb109225f14b80622ea
Siddhartha Basu <biosidd@gmail.com>
2023-11-13 08:05:52 -0600

    refactor(role.go): change the way permissions are fetched and processed
    The previous method of fetching permissions was replaced with a more
    efficient one, which directly calls the Permissions method with the role
    ID. This change simplifies the code and improves performance.
    Additionally, error handling and logging were enhanced to provide more
    detailed information in case of an error.

--- internal/graphql/resolver/user/role.go
+++ internal/graphql/resolver/user/role.go
@@ -2,14 +2,14 @@ package user
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"time"
 
 	"github.com/dictyBase/aphgrpc"
-	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/user"
-	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
 	"github.com/dictyBase/graphql-server/internal/authentication"
+	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,44 +21,83 @@ type RoleResolver struct {
 func (rrs *RoleResolver) ID(ctx context.Context, obj *pb.Role) (string, error) {
 	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
-func (rrs *RoleResolver) Role(ctx context.Context, obj *pb.Role) (string, error) {
+
+func (rrs *RoleResolver) Role(
+	ctx context.Context,
+	obj *pb.Role,
+) (string, error) {
 	return obj.Data.Attributes.Role, nil
 }
-func (rrs *RoleResolver) Description(ctx context.Context, obj *pb.Role) (string, error) {
+
+func (rrs *RoleResolver) Description(
+	ctx context.Context,
+	obj *pb.Role,
+) (string, error) {
 	return obj.Data.Attributes.Description, nil
 }
-func (rrs *RoleResolver) CreatedAt(ctx context.Context, obj *pb.Role) (*time.Time, error) {
+
+func (rrs *RoleResolver) CreatedAt(
+	ctx context.Context,
+	obj *pb.Role,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt)
 	return &time, nil
 }
-func (rrs *RoleResolver) UpdatedAt(ctx context.Context, obj *pb.Role) (*time.Time, error) {
+
+func (rrs *RoleResolver) UpdatedAt(
+	ctx context.Context,
+	obj *pb.Role,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt)
 	return &time, nil
 }
-func (rrs *RoleResolver) Permissions(ctx context.Context, obj *pb.Role) ([]*pb.Permission, error) {
+
+func (rrs *RoleResolver) Permissions(
+	ctx context.Context,
+	obj *pb.Role,
+) ([]*pb.Permission, error) {
 	permissions := []*pb.Permission{}
-	rp, err := rrs.Client.GetRelatedPermissions(ctx, &jsonapi.RelationshipRequest{Id: obj.Data.Id})
+	roleId := strconv.FormatInt(obj.Data.Id, 10)
+	permResp, err := rrs.Client.Permissions(roleId)
 	if err != nil {
-		errorutils.AddGQLError(ctx, err)
+		errorutils.AddGQLError(
+			ctx,
+			fmt.Errorf("error in fetching permissions", err),
+		)
 		rrs.Logger.Error(err)
 		return permissions, err
 	}
-	for _, n := range rp.Data {
+	for _, perm := range permResp {
+		permId, err := strconv.ParseInt(perm.ID, 10, 64)
+		if err != nil {
+			errorutils.AddGQLError(
+				ctx,
+				fmt.Errorf(
+					"error in converting permission id to integer %s",
+					err,
+				),
+			)
+			rrs.Logger.Error(err)
+			return permissions, err
+		}
 		item := &pb.Permission{
 			Data: &pb.PermissionData{
 				Type: "permission",
-				Id:   n.Id,
+				Id:   permId,
 				Attributes: &pb.PermissionAttributes{
-					Permission:  n.Attributes.Permission,
-					Description: n.Attributes.Description,
-					CreatedAt:   n.Attributes.CreatedAt,
-					UpdatedAt:   n.Attributes.UpdatedAt,
-					Resource:    n.Attributes.Resource,
+					Permission:  perm.Name,
+					Description: perm.Description,
+					Resource:    perm.Resource.Name,
 				},
 			},
 		}
 		permissions = append(permissions, item)
 	}
-	rrs.Logger.Infof("successfully retrieved list of %d permissions for role ID %d", len(permissions), obj.Data.Id)
+	rrs.Logger.Infof(
+		"successfully retrieved list of %d permissions for role ID %d",
+		len(permissions),
+		obj.Data.Id,
+	)
+
 	return permissions, nil
 }
