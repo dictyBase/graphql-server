de8d71c0b10e4fdf1b1d1dba21da21874b242a53
Siddhartha Basu <biosidd@gmail.com>
2023-11-13 08:55:55 -0600

    feat(content.go): replace user id with email for user retrieval
    The user retrieval method has been updated to use email instead of id,
    providing a more user-friendly way to identify users. Additionally,
    error handling has been improved to provide more specific error messages
    when user retrieval or user id conversion fails.

--- internal/graphql/resolver/content/content.go
+++ internal/graphql/resolver/content/content.go
@@ -2,6 +2,7 @@ package content
 
 import (
 	"context"
+	"fmt"
 	"strconv"
 	"time"
 
@@ -9,8 +10,8 @@ import (
 	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/content"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
-	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
 	"github.com/dictyBase/graphql-server/internal/authentication"
+	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
 	"github.com/sirupsen/logrus"
 )
 
@@ -30,16 +31,39 @@ func (rcs *ContentResolver) Slug(ctx context.Context, obj *pb.Content) (string, 
 	return obj.Data.Attributes.Slug, nil
 }
 func (rcs *ContentResolver) CreatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
-	user := user.User{}
-	id := obj.Data.Attributes.CreatedBy
-	g, err := rcs.UserClient.GetUser(ctx, &jsonapi.GetRequest{Id: id})
+	email :=  obj.Data.Attributes.CreatedBy
+	userResp, err := rcs.UserClient.UserWithEmail(email)
 	if err != nil {
-		errorutils.AddGQLError(ctx, err)
+		errorutils.AddGQLError(ctx, fmt.Errorf("unable to retreieve user %s",err))
 		rcs.Logger.Error(err)
-		return &user, err
+		return nil, err
 	}
-	rcs.Logger.Debugf("successfully found user with id %d", id)
-	return g, nil
+	userId,err:=strconv.ParseInt(userResp.ID,10,64)
+	if err != nil {
+		errorutils.AddGQLError(ctx, fmt.Errorf("unable to convert user id to integer %s",err))
+		rcs.Logger.Error(err)
+		return nil, err
+	}
+	rcs.Logger.Debugf("successfully found user with id %d", email)
+	return &user.User{
+		Data: &user.UserData{
+			Type: "user",
+			Id:   userId,
+			Attributes: &user.UserAttributes{
+				FirstName:    userResp.Username,
+				LastName:     userResp.Name,
+				Email:        userResp.PrimaryEmail,
+				Organization: userResp.CustomData.Institution,
+				FirstAddress: userResp.CustomData.Address,
+				City:         userResp.CustomData.City,
+				State:        userResp.CustomData.State,
+				Zipcode:      userResp.CustomData.Zipcode,
+				Country:      userResp.CustomData.Country,
+				Phone:        userResp.PrimaryPhone,
+				IsActive:     true,
+			},
+		},
+	}, nil
 }
 func (rcs *ContentResolver) UpdatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
 	user := user.User{}
