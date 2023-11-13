08da75901efa75fcb67d6d35866f21e1d22c3834
Siddhartha Basu <biosidd@gmail.com>
2023-11-13 08:57:55 -0600

    refactor(content.go): add detailed user information to UpdatedBy method
    The unused import
    'github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi' was removed
    to clean up the code. The method of retrieving a user was updated from
    using an ID to using an email, which is more reliable and user-friendly.
    The UpdatedBy method now returns detailed user information, providing a
    more comprehensive user profile.

--- internal/graphql/resolver/content/content.go
+++ internal/graphql/resolver/content/content.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/dictyBase/aphgrpc"
-	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/content"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
 	"github.com/dictyBase/graphql-server/internal/authentication"
@@ -21,30 +20,51 @@ type ContentResolver struct {
 	Logger     *logrus.Entry
 }
 
-func (rcs *ContentResolver) ID(ctx context.Context, obj *pb.Content) (string, error) {
+func (rcs *ContentResolver) ID(
+	ctx context.Context,
+	obj *pb.Content,
+) (string, error) {
 	return strconv.FormatInt(obj.Data.Id, 10), nil
 }
-func (rcs *ContentResolver) Name(ctx context.Context, obj *pb.Content) (string, error) {
+
+func (rcs *ContentResolver) Name(
+	ctx context.Context,
+	obj *pb.Content,
+) (string, error) {
 	return obj.Data.Attributes.Name, nil
 }
-func (rcs *ContentResolver) Slug(ctx context.Context, obj *pb.Content) (string, error) {
+
+func (rcs *ContentResolver) Slug(
+	ctx context.Context,
+	obj *pb.Content,
+) (string, error) {
 	return obj.Data.Attributes.Slug, nil
 }
-func (rcs *ContentResolver) CreatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
-	email :=  obj.Data.Attributes.CreatedBy
+
+func (rcs *ContentResolver) CreatedBy(
+	ctx context.Context,
+	obj *pb.Content,
+) (*user.User, error) {
+	email := obj.Data.Attributes.CreatedBy
 	userResp, err := rcs.UserClient.UserWithEmail(email)
 	if err != nil {
-		errorutils.AddGQLError(ctx, fmt.Errorf("unable to retreieve user %s",err))
+		errorutils.AddGQLError(
+			ctx,
+			fmt.Errorf("unable to retreieve user %s", err),
+		)
 		rcs.Logger.Error(err)
 		return nil, err
 	}
-	userId,err:=strconv.ParseInt(userResp.ID,10,64)
+	userId, err := strconv.ParseInt(userResp.ID, 10, 64)
 	if err != nil {
-		errorutils.AddGQLError(ctx, fmt.Errorf("unable to convert user id to integer %s",err))
+		errorutils.AddGQLError(
+			ctx,
+			fmt.Errorf("unable to convert user id to integer %s", err),
+		)
 		rcs.Logger.Error(err)
 		return nil, err
 	}
-	rcs.Logger.Debugf("successfully found user with id %d", email)
+	rcs.Logger.Debugf("successfully found user with id %s", email)
 	return &user.User{
 		Data: &user.UserData{
 			Type: "user",
@@ -65,29 +85,78 @@ func (rcs *ContentResolver) CreatedBy(ctx context.Context, obj *pb.Content) (*us
 		},
 	}, nil
 }
-func (rcs *ContentResolver) UpdatedBy(ctx context.Context, obj *pb.Content) (*user.User, error) {
-	user := user.User{}
-	id := obj.Data.Attributes.UpdatedBy
-	g, err := rcs.UserClient.GetUser(ctx, &jsonapi.GetRequest{Id: id})
+
+func (rcs *ContentResolver) UpdatedBy(
+	ctx context.Context,
+	obj *pb.Content,
+) (*user.User, error) {
+	email := obj.Data.Attributes.UpdatedBy
+	userResp, err := rcs.UserClient.UserWithEmail(email)
 	if err != nil {
-		errorutils.AddGQLError(ctx, err)
+		errorutils.AddGQLError(
+			ctx,
+			fmt.Errorf("unable to retreieve user %s", err),
+		)
 		rcs.Logger.Error(err)
-		return &user, err
+		return nil, err
 	}
-	rcs.Logger.Debugf("successfully found user with id %d", id)
-	return g, nil
+	userId, err := strconv.ParseInt(userResp.ID, 10, 64)
+	if err != nil {
+		errorutils.AddGQLError(
+			ctx,
+			fmt.Errorf("unable to convert user id to integer %s", err),
+		)
+		rcs.Logger.Error(err)
+		return nil, err
+	}
+	rcs.Logger.Debugf("successfully found user with id %s", email)
+	return &user.User{
+		Data: &user.UserData{
+			Type: "user",
+			Id:   userId,
+			Attributes: &user.UserAttributes{
+				FirstName:    userResp.Username,
+				LastName:     userResp.Name,
+				Email:        userResp.PrimaryEmail,
+				Organization: userResp.CustomData.Institution,
+				FirstAddress: userResp.CustomData.Address,
+				City:         userResp.CustomData.City,
+				State:        userResp.CustomData.State,
+				Zipcode:      userResp.CustomData.Zipcode,
+				Country:      userResp.CustomData.Country,
+				Phone:        userResp.PrimaryPhone,
+				IsActive:     true,
+			},
+		},
+	}, nil
 }
-func (rcs *ContentResolver) CreatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
+
+func (rcs *ContentResolver) CreatedAt(
+	ctx context.Context,
+	obj *pb.Content,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt)
 	return &time, nil
 }
-func (rcs *ContentResolver) UpdatedAt(ctx context.Context, obj *pb.Content) (*time.Time, error) {
+
+func (rcs *ContentResolver) UpdatedAt(
+	ctx context.Context,
+	obj *pb.Content,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt)
 	return &time, nil
 }
-func (rcs *ContentResolver) Content(ctx context.Context, obj *pb.Content) (string, error) {
+
+func (rcs *ContentResolver) Content(
+	ctx context.Context,
+	obj *pb.Content,
+) (string, error) {
 	return obj.Data.Attributes.Content, nil
 }
-func (rcs *ContentResolver) Namespace(ctx context.Context, obj *pb.Content) (string, error) {
+
+func (rcs *ContentResolver) Namespace(
+	ctx context.Context,
+	obj *pb.Content,
+) (string, error) {
 	return obj.Data.Attributes.Namespace, nil
 }
