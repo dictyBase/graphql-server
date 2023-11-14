48da858290b815fe5abc454d4cc97374fe823ba9
Siddhartha Basu <biosidd@gmail.com>
2023-11-13 18:38:19 -0600

    refactor(verify.go): extract token claim checks and role/scope checks into separate functions for better code readability and reusability
    The token claim checks and role/scope checks were previously done within
    the CheckCreateContent and CheckUpdateContent functions. These checks
    have now been extracted into separate functions (checkTokenClaims,
    checkRole, checkScope) to improve code readability and reusability.

--- internal/authentication/verify.go
+++ internal/authentication/verify.go
@@ -44,60 +44,82 @@ func CheckReadUser(ctx context.Context) error {
 }
 
 func CheckCreateContent(ctx context.Context) error {
-	token, err := HasToken(ctx)
+	roles, scopes, err := checkTokenClaims(ctx, "roles", "scopes")
 	if err != nil {
 		return err
 	}
+
+	err = checkRole(roles, contentCreatorRole)
+	if err != nil {
+		return err
+	}
+
+	err = checkScope(scopes, contentCreatorScope)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
+func CheckUpdateContent(ctx context.Context) error {
+	roles, scopes, err := checkTokenClaims(ctx, "roles", "scopes")
+	if err != nil {
+		return err
+	}
+	
+	err = checkRole(roles, contentEditorRole)
+	if err != nil {
+		return err
+	}
+
+	err = checkScope(scopes, contentEditorScope)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkTokenClaims(
+	ctx context.Context,
+	roleSlot string,
+	scopeSlot string,
+) (string, string, error) {
+	token, err := HasToken(ctx)
+	if err != nil {
+		return "", "", err
+	}
 	claims := token.PrivateClaims()
-	for _, clm := range []string{"roles", "scopes"} {
+	for _, clm := range []string{roleSlot, scopeSlot} {
 		if _, ok := claims[clm]; !ok {
-			return fmt.Errorf("query without claim %s not allowed", err)
+			return "", "", fmt.Errorf("query without claim %s not allowed", clm)
 		}
 	}
-	roles := fmt.Sprintf("%v", claims["roles"])
+	roles := fmt.Sprintf("%v", claims[roleSlot])
+	scopes := fmt.Sprintf("%v", claims[scopeSlot])
+
+	return roles, scopes, nil
+}
+
+func checkRole(roles string, expectedRoles []string) error {
 	rolesOk := false
-	for _, rls := range contentCreatorRole {
+	for _, rls := range expectedRoles {
 		if strings.Contains(roles, rls) {
 			rolesOk = true
 			break
 		}
 	}
 	if !rolesOk {
-		return errors.New("query without content creator role is not allowed")
-	}
-	scopes := fmt.Sprintf("%v", claims["scopes"])
-	if !strings.Contains(scopes, contentCreatorScope) {
-		return errors.New("query without content writing scope is not allowed")
+		return errors.New("query without proper role is not allowed")
 	}
 	return nil
 }
 
-func CheckUpdateContent(ctx context.Context) error {
-	token, err := HasToken(ctx)
-	if err != nil {
-		return err
+func checkScope(scopes string, expectedScope string) error {
+	if !strings.Contains(scopes, expectedScope) {
+		return errors.New("query without proper scope is not allowed")
 	}
-	claims := token.PrivateClaims()
-	for _, clm := range []string{"roles", "scopes"} {
-		if _, ok := claims[clm]; !ok {
-			return fmt.Errorf("query without claim %s not allowed", err)
-		}
-	}
-	roles := fmt.Sprintf("%v", claims["roles"])
-	rolesOk := false
-	for _, rls := range contentEditorRole {
-		if strings.Contains(roles, rls) {
-			rolesOk = true
-			break
-		}
-	}
-	if !rolesOk {
-		return errors.New("query without content editor role is not allowed")
-	}
-	scopes := fmt.Sprintf("%v", claims["scopes"])
-	if !strings.Contains(scopes, contentEditorScope) {
-		return errors.New("query without content editing scope is not allowed")
-	}
-
 	return nil
 }
