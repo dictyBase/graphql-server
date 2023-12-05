d06c763af59576e818e0cc7a4791c6b9c2a333f9
Siddhartha Basu <biosidd@gmail.com>
2023-12-05 15:19:13 -0600

    refactor(auth.go): replace old auth client with new LogtoClient and simplify mockAuth function
    The old auth client and its associated mockAuth function were replaced
    with a new LogtoClient. This change simplifies the mockAuth function by
    removing unnecessary details and focusing on the essential
    functionality.

--- internal/graphql/mocks/auth.go
+++ internal/graphql/mocks/auth.go
@@ -1,54 +1,16 @@
 package mocks
 
 import (
-	"github.com/dictyBase/go-genproto/dictybaseapis/auth"
-	"github.com/dictyBase/go-genproto/dictybaseapis/identity"
-	"github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"github.com/dictyBase/graphql-server/internal/authentication"
 	"github.com/dictyBase/graphql-server/internal/graphql/mocks/clients"
-	"github.com/golang/protobuf/ptypes"
 	"github.com/stretchr/testify/mock"
 )
 
-func mockAuth() *auth.Auth {
-	return &auth.Auth{
-		Token:        "test token",
-		RefreshToken: "test refresh token",
-		Identity: &identity.Identity{
-			Data: &identity.IdentityData{
-				Type: "identity",
-				Id:   123,
-				Attributes: &identity.IdentityAttributes{
-					Identifier: "art@vandelay.com",
-					Provider:   "google",
-					UserId:     999,
-					CreatedAt:  ptypes.TimestampNow(),
-					UpdatedAt:  ptypes.TimestampNow(),
-				},
-			},
-		},
-		User: &user.User{
-			Data: &user.UserData{
-				Type: "user",
-				Id:   999,
-				Attributes: &user.UserAttributes{
-					FirstName: "Art",
-					LastName:  "Vandelay",
-					Email:     "art@vandelay.com",
-					IsActive:  true,
-					CreatedAt: ptypes.TimestampNow(),
-					UpdatedAt: ptypes.TimestampNow(),
-				},
-			},
-		},
-	}
-}
-
-func MockedAuthClient() *clients.AuthServiceClient {
-	mockedAuthClient := new(clients.AuthServiceClient)
+func MockedAuthClient() authentication.LogtoClient {
+	mockedAuthClient := new(clients.LogtoClient)
 	mockedAuthClient.On(
-		"Login",
-		mock.AnythingOfType("*context.valueCtx"),
-		mock.AnythingOfType("*auth.NewLogin"),
-	).Return(mockAuth(), nil)
+		"CheckUser",
+		mock.AnythingOfType("string"),
+	).Return(true, "", nil)
 	return mockedAuthClient
 }
