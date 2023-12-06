3f716108ffaaefc5b3ce8ff8b434e2e70d813a7c
Siddhartha Basu <biosidd@gmail.com>
2023-12-06 14:59:15 -0600

    style(permission.go): improve code readability by breaking down long function signatures and calls into multiple lines
    The changes were made to improve the readability of the code. Long
    function signatures and calls were broken down into multiple lines,
    making it easier to understand the structure and flow of the code. This
    is particularly beneficial for developers who are new to the codebase or
    when reviewing changes in the future.

--- internal/graphql/resolver/permission.go
+++ internal/graphql/resolver/permission.go
@@ -15,17 +15,21 @@ import (
 	"github.com/dictyBase/graphql-server/internal/registry"
 )
 
-func (m *MutationResolver) CreatePermission(ctx context.Context, input *models.CreatePermissionInput) (*pb.Permission, error) {
-	n, err := m.GetPermissionClient(registry.PERMISSION).CreatePermission(ctx, &pb.CreatePermissionRequest{
-		Data: &pb.CreatePermissionRequest_Data{
-			Type: "permission",
-			Attributes: &pb.PermissionAttributes{
-				Permission:  input.Permission,
-				Description: input.Description,
-				Resource:    input.Resource,
+func (m *MutationResolver) CreatePermission(
+	ctx context.Context,
+	input *models.CreatePermissionInput,
+) (*pb.Permission, error) {
+	n, err := m.GetPermissionClient(registry.PERMISSION).
+		CreatePermission(ctx, &pb.CreatePermissionRequest{
+			Data: &pb.CreatePermissionRequest_Data{
+				Type: "permission",
+				Attributes: &pb.PermissionAttributes{
+					Permission:  input.Permission,
+					Description: input.Description,
+					Resource:    input.Resource,
+				},
 			},
-		},
-	})
+		})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		m.Logger.Error(err)
@@ -34,30 +38,37 @@ func (m *MutationResolver) CreatePermission(ctx context.Context, input *models.C
 	m.Logger.Debugf("successfully created new permission with ID %d", n.Data.Id)
 	return n, nil
 }
-func (m *MutationResolver) UpdatePermission(ctx context.Context, id string, input *models.UpdatePermissionInput) (*pb.Permission, error) {
+
+func (m *MutationResolver) UpdatePermission(
+	ctx context.Context,
+	id string,
+	input *models.UpdatePermissionInput,
+) (*pb.Permission, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("error in parsing string %s to int %s", id, err)
 	}
-	n, err := m.GetPermissionClient(registry.PERMISSION).UpdatePermission(ctx, &pb.UpdatePermissionRequest{
-		Id: i,
-		Data: &pb.UpdatePermissionRequest_Data{
-			Id:   i,
-			Type: "permission",
-			Attributes: &pb.PermissionAttributes{
-				Permission:  input.Permission,
-				Description: input.Description,
-				Resource:    input.Resource,
-				UpdatedAt:   aphgrpc.TimestampProto(time.Now()),
+	n, err := m.GetPermissionClient(registry.PERMISSION).
+		UpdatePermission(ctx, &pb.UpdatePermissionRequest{
+			Id: i,
+			Data: &pb.UpdatePermissionRequest_Data{
+				Id:   i,
+				Type: "permission",
+				Attributes: &pb.PermissionAttributes{
+					Permission:  input.Permission,
+					Description: input.Description,
+					Resource:    input.Resource,
+					UpdatedAt:   aphgrpc.TimestampProto(time.Now()),
+				},
 			},
-		},
-	})
+		})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		m.Logger.Error(err)
 		return nil, err
 	}
-	o, err := m.GetPermissionClient(registry.PERMISSION).GetPermission(ctx, &jsonapi.GetRequestWithFields{Id: n.Data.Id})
+	o, err := m.GetPermissionClient(registry.PERMISSION).
+		GetPermission(ctx, &jsonapi.GetRequestWithFields{Id: n.Data.Id})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		m.Logger.Error(err)
@@ -66,12 +77,19 @@ func (m *MutationResolver) UpdatePermission(ctx context.Context, id string, inpu
 	m.Logger.Debugf("successfully updated permission with ID %d", o.Data.Id)
 	return o, nil
 }
-func (m *MutationResolver) DeletePermission(ctx context.Context, id string) (*models.DeletePermission, error) {
+
+func (m *MutationResolver) DeletePermission(
+	ctx context.Context,
+	id string,
+) (*models.DeletePermission, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("error in parsing string %s to int %s", id, err)
 	}
-	if _, err := m.GetPermissionClient(registry.PERMISSION).DeletePermission(ctx, &jsonapi.DeleteRequest{Id: i}); err != nil {
+
+	_, err = m.GetPermissionClient(registry.PERMISSION).
+		DeletePermission(ctx, &jsonapi.DeleteRequest{Id: i})
+	if err != nil {
 		m.Logger.Error(err)
 		return &models.DeletePermission{
 			Success: false,
@@ -83,12 +101,16 @@ func (m *MutationResolver) DeletePermission(ctx context.Context, id string) (*mo
 	}, nil
 }
 
-func (q *QueryResolver) Permission(ctx context.Context, id string) (*pb.Permission, error) {
+func (q *QueryResolver) Permission(
+	ctx context.Context,
+	id string,
+) (*pb.Permission, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("error in parsing string %s to int %s", id, err)
 	}
-	g, err := q.GetPermissionClient(registry.PERMISSION).GetPermission(ctx, &jsonapi.GetRequestWithFields{Id: i})
+	g, err := q.GetPermissionClient(registry.PERMISSION).
+		GetPermission(ctx, &jsonapi.GetRequestWithFields{Id: i})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		q.Logger.Error(err)
@@ -97,9 +119,13 @@ func (q *QueryResolver) Permission(ctx context.Context, id string) (*pb.Permissi
 	q.Logger.Debugf("successfully found permission with ID %s", id)
 	return g, nil
 }
-func (q *QueryResolver) ListPermissions(ctx context.Context) ([]*pb.Permission, error) {
+
+func (q *QueryResolver) ListPermissions(
+	ctx context.Context,
+) ([]*pb.Permission, error) {
 	permissions := []*pb.Permission{}
-	l, err := q.GetPermissionClient(registry.PERMISSION).ListPermissions(ctx, &jsonapi.SimpleListRequest{})
+	l, err := q.GetPermissionClient(registry.PERMISSION).
+		ListPermissions(ctx, &jsonapi.SimpleListRequest{})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		q.Logger.Error(err)
@@ -121,6 +147,9 @@ func (q *QueryResolver) ListPermissions(ctx context.Context) ([]*pb.Permission, 
 		}
 		permissions = append(permissions, item)
 	}
-	q.Logger.Debugf("successfully provided list of %d permissions", len(permissions))
+	q.Logger.Debugf(
+		"successfully provided list of %d permissions",
+		len(permissions),
+	)
 	return permissions, nil
 }
