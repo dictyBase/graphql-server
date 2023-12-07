beafd9d3a3293c9ed3b1f6b92272825a7bc16d49
Siddhartha Basu <biosidd@gmail.com>
2023-12-07 13:36:39 -0600

    feat(user.go): add nolint directive to suppress linting warning for CreateUserRoleRelationship function
    The nolint directive was added to suppress linting warnings for the
    CreateUserRoleRelationship function. This is necessary because the
    function's complexity or length might trigger linting warnings, but in
    this case, it's acceptable and doesn't indicate an actual issue.

--- internal/graphql/models/stock.go
+++ internal/graphql/models/stock.go
@@ -25,18 +25,18 @@ type Strain struct {
 	UpdatedAt           time.Time `json:"updated_at"`
 	Summary             *string   `json:"summary"`
 	EditableSummary     *string   `json:"editable_summary"`
-	Dbxrefs             []string `json:"dbxrefs"`
+	Dbxrefs             []string  `json:"dbxrefs"`
 	SystematicName      string    `json:"systematic_name"`
 	Label               string    `json:"label"`
 	Species             string    `json:"species"`
 	Plasmid             *string   `json:"plasmid"`
 	Parent              *string   `json:"parent"`
-	Names               []string `json:"names"`
+	Names               []string  `json:"names"`
 	InStock             bool      `json:"in_stock"`
 	GeneticModification *string   `json:"genetic_modification"`
 	MutagenesisMethod   *string   `json:"mutagenesis_method"`
-	Characteristics     []string `json:"characteristics"`
-	Genotypes           []string `json:"genotypes"`
+	Characteristics     []string  `json:"characteristics"`
+	Genotypes           []string  `json:"genotypes"`
 }
 
 type Plasmid struct {
