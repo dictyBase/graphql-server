0a193af6496c4167e40fd817e107e9cc548fff74
Siddhartha Basu <biosidd@gmail.com>
2023-12-07 12:27:53 -0600

    style(role.go): improve code readability by breaking down long function signatures and calls into multiple lines
    The changes were made to improve the readability of the code. Long
    function signatures and calls were broken down into multiple lines,
    making it easier to understand the function parameters and the structure
    of the function calls.

--- internal/graphql/resolver/role.go
+++ internal/graphql/resolver/role.go
@@ -15,16 +15,20 @@ import (
 	"github.com/dictyBase/graphql-server/internal/registry"
 )
 
-func (mrs *MutationResolver) CreateRole(ctx context.Context, input *models.CreateRoleInput) (*pb.Role, error) {
-	n, err := mrs.GetRoleClient(registry.ROLE).CreateRole(ctx, &pb.CreateRoleRequest{
-		Data: &pb.CreateRoleRequest_Data{
-			Type: "role",
-			Attributes: &pb.RoleAttributes{
-				Role:        input.Role,
-				Description: input.Description,
+func (mrs *MutationResolver) CreateRole(
+	ctx context.Context,
+	input *models.CreateRoleInput,
+) (*pb.Role, error) {
+	n, err := mrs.GetRoleClient(registry.ROLE).
+		CreateRole(ctx, &pb.CreateRoleRequest{
+			Data: &pb.CreateRoleRequest_Data{
+				Type: "role",
+				Attributes: &pb.RoleAttributes{
+					Role:        input.Role,
+					Description: input.Description,
+				},
 			},
-		},
-	})
+		})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		mrs.Logger.Error(err)
@@ -33,30 +37,50 @@ func (mrs *MutationResolver) CreateRole(ctx context.Context, input *models.Creat
 	mrs.Logger.Debugf("successfully created new role with ID %d", n.Data.Id)
 	return n, nil
 }
-func (mrs *MutationResolver) CreateRolePermissionRelationship(ctx context.Context, roleId string, permissionId string) (*pb.Role, error) {
-	rid, err := strconv.ParseInt(roleId, 10, 64)
+
+func (mrs *MutationResolver) CreateRolePermissionRelationship(
+	ctx context.Context,
+	roleID string,
+	permissionID string,
+) (*pb.Role, error) {
+	rid, err := strconv.ParseInt(roleID, 10, 64)
 	if err != nil {
-		return nil, fmt.Errorf("error in parsing string %s to int %s", roleId, err)
+		return nil, fmt.Errorf(
+			"error in parsing string %s to int %s",
+			roleID,
+			err,
+		)
 	}
-	pid, err := strconv.ParseInt(permissionId, 10, 64)
+	pid, err := strconv.ParseInt(permissionID, 10, 64)
 	if err != nil {
-		return nil, fmt.Errorf("error in parsing string %s to int %s", permissionId, err)
+		return nil, fmt.Errorf(
+			"error in parsing string %s to int %s",
+			permissionID,
+			err,
+		)
 	}
-	rr, err := mrs.GetRoleClient(registry.ROLE).CreatePermissionRelationship(ctx, &jsonapi.DataCollection{
-		Id: rid,
-		Data: []*jsonapi.Data{
-			{
-				Type: "permission",
-				Id:   pid,
-			},
-		}})
+	rr, err := mrs.GetRoleClient(registry.ROLE).
+		CreatePermissionRelationship(ctx, &jsonapi.DataCollection{
+			Id: rid,
+			Data: []*jsonapi.Data{
+				{
+					Type: "permission",
+					Id:   pid,
+				},
+			}})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		mrs.Logger.Error(err)
 		return nil, err
 	}
-	mrs.Logger.Debugf("successfully created role ID %d relationship permission with ID %d %s", rid, pid, rr)
-	g, err := mrs.GetRoleClient(registry.ROLE).GetRole(ctx, &jsonapi.GetRequest{Id: rid})
+	mrs.Logger.Debugf(
+		"successfully created role ID %d relationship permission with ID %d %s",
+		rid,
+		pid,
+		rr,
+	)
+	g, err := mrs.GetRoleClient(registry.ROLE).
+		GetRole(ctx, &jsonapi.GetRequest{Id: rid})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		mrs.Logger.Error(err)
@@ -64,29 +88,36 @@ func (mrs *MutationResolver) CreateRolePermissionRelationship(ctx context.Contex
 	}
 	return g, nil
 }
-func (mrs *MutationResolver) UpdateRole(ctx context.Context, id string, input *models.UpdateRoleInput) (*pb.Role, error) {
+
+func (mrs *MutationResolver) UpdateRole(
+	ctx context.Context,
+	id string,
+	input *models.UpdateRoleInput,
+) (*pb.Role, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("error in parsing string %s to int %s", id, err)
 	}
-	n, err := mrs.GetRoleClient(registry.ROLE).UpdateRole(ctx, &pb.UpdateRoleRequest{
-		Id: i,
-		Data: &pb.UpdateRoleRequest_Data{
-			Id:   i,
-			Type: "role",
-			Attributes: &pb.RoleAttributes{
-				Role:        input.Role,
-				Description: input.Description,
-				UpdatedAt:   aphgrpc.TimestampProto(time.Now()),
+	n, err := mrs.GetRoleClient(registry.ROLE).
+		UpdateRole(ctx, &pb.UpdateRoleRequest{
+			Id: i,
+			Data: &pb.UpdateRoleRequest_Data{
+				Id:   i,
+				Type: "role",
+				Attributes: &pb.RoleAttributes{
+					Role:        input.Role,
+					Description: input.Description,
+					UpdatedAt:   aphgrpc.TimestampProto(time.Now()),
+				},
 			},
-		},
-	})
+		})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		mrs.Logger.Error(err)
 		return nil, err
 	}
-	o, err := mrs.GetRoleClient(registry.ROLE).GetRole(ctx, &jsonapi.GetRequest{Id: n.Data.Id})
+	o, err := mrs.GetRoleClient(registry.ROLE).
+		GetRole(ctx, &jsonapi.GetRequest{Id: n.Data.Id})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		mrs.Logger.Error(err)
@@ -95,7 +126,11 @@ func (mrs *MutationResolver) UpdateRole(ctx context.Context, id string, input *m
 	mrs.Logger.Debugf("successfully updated role with ID %d", o.Data.Id)
 	return o, nil
 }
-func (mrs *MutationResolver) DeleteRole(ctx context.Context, id string) (*models.DeleteRole, error) {
+
+func (mrs *MutationResolver) DeleteRole(
+	ctx context.Context,
+	id string,
+) (*models.DeleteRole, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("error in parsing string %s to int %s", id, err)
@@ -111,12 +146,17 @@ func (mrs *MutationResolver) DeleteRole(ctx context.Context, id string) (*models
 		Success: true,
 	}, nil
 }
-func (qrs *QueryResolver) Role(ctx context.Context, id string) (*pb.Role, error) {
+
+func (qrs *QueryResolver) Role(
+	ctx context.Context,
+	id string,
+) (*pb.Role, error) {
 	i, err := strconv.ParseInt(id, 10, 64)
 	if err != nil {
 		return nil, fmt.Errorf("error in parsing string %s to int %s", id, err)
 	}
-	g, err := qrs.GetRoleClient(registry.ROLE).GetRole(ctx, &jsonapi.GetRequest{Id: i})
+	g, err := qrs.GetRoleClient(registry.ROLE).
+		GetRole(ctx, &jsonapi.GetRequest{Id: i})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		qrs.Logger.Error(err)
@@ -127,7 +167,8 @@ func (qrs *QueryResolver) Role(ctx context.Context, id string) (*pb.Role, error)
 }
 func (qrs *QueryResolver) ListRoles(ctx context.Context) ([]*pb.Role, error) {
 	roles := []*pb.Role{}
-	l, err := qrs.GetRoleClient(registry.ROLE).ListRoles(ctx, &jsonapi.SimpleListRequest{})
+	l, err := qrs.GetRoleClient(registry.ROLE).
+		ListRoles(ctx, &jsonapi.SimpleListRequest{})
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
 		qrs.Logger.Error(err)
