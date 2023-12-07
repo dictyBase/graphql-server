cd80888df935682426b89da85de3b8c0e52cb452
Siddhartha Basu <biosidd@gmail.com>
2023-12-07 12:48:04 -0600

    refactor(content.go): simplify user retrieval by email and error handling
    The functions 'CreatedBy' and 'UpdatedBy' have been refactored to use a
    new helper function 'userByEmail' which retrieves a user by email. This
    reduces code duplication and makes the code more maintainable. The error
    handling has also been simplified to return the error directly instead
    of logging and adding it to GraphQL errors. This change makes the error
    handling more straightforward and less prone to mistakes.

--- internal/graphql/resolver/content/content.go
+++ internal/graphql/resolver/content/content.go
@@ -41,11 +41,10 @@ func (rcs *ContentResolver) Slug(
 	return obj.Data.Attributes.Slug, nil
 }
 
-func (rcs *ContentResolver) CreatedBy(
+func (rcs *ContentResolver) userByEmail(
 	ctx context.Context,
-	obj *pb.Content,
+	email string,
 ) (*user.User, error) {
-	email := obj.Data.Attributes.CreatedBy
 	userResp, err := rcs.UserClient.UserWithEmail(email)
 	if err != nil {
 		errorutils.AddGQLError(
@@ -86,49 +85,26 @@ func (rcs *ContentResolver) CreatedBy(
 	}, nil
 }
 
+func (rcs *ContentResolver) CreatedBy(
+	ctx context.Context,
+	obj *pb.Content,
+) (*user.User, error) {
+	user, err := rcs.userByEmail(ctx, obj.Data.Attributes.CreatedBy)
+	if err != nil {
+		return user, fmt.Errorf("error in getting created by user %s", err)
+	}
+	return user, nil
+}
+
 func (rcs *ContentResolver) UpdatedBy(
 	ctx context.Context,
 	obj *pb.Content,
 ) (*user.User, error) {
-	email := obj.Data.Attributes.UpdatedBy
-	userResp, err := rcs.UserClient.UserWithEmail(email)
+	user, err := rcs.userByEmail(ctx, obj.Data.Attributes.CreatedBy)
 	if err != nil {
-		errorutils.AddGQLError(
-			ctx,
-			fmt.Errorf("unable to retreieve user %s", err),
-		)
-		rcs.Logger.Error(err)
-		return nil, err
+		return user, fmt.Errorf("error in getting updated by user %s", err)
 	}
-	userID, err := strconv.ParseInt(userResp.ID, 10, 64)
-	if err != nil {
-		errorutils.AddGQLError(
-			ctx,
-			fmt.Errorf("unable to convert user id to integer %s", err),
-		)
-		rcs.Logger.Error(err)
-		return nil, err
-	}
-	rcs.Logger.Debugf("successfully found user with id %s", email)
-	return &user.User{
-		Data: &user.UserData{
-			Type: "user",
-			Id:   userID,
-			Attributes: &user.UserAttributes{
-				FirstName:    userResp.Username,
-				LastName:     userResp.Name,
-				Email:        userResp.PrimaryEmail,
-				Organization: userResp.CustomData.Institution,
-				FirstAddress: userResp.CustomData.Address,
-				City:         userResp.CustomData.City,
-				State:        userResp.CustomData.State,
-				Zipcode:      userResp.CustomData.Zipcode,
-				Country:      userResp.CustomData.Country,
-				Phone:        userResp.PrimaryPhone,
-				IsActive:     true,
-			},
-		},
-	}, nil
+	return user, nil
 }
 
 func (rcs *ContentResolver) CreatedAt(
