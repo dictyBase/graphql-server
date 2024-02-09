ad7046c89907305ab2554c050982da9b8ea47c59
Siddhartha Basu <biosidd@gmail.com>
2024-02-09 12:51:05 -0600

    feat(authentication): add ValidateContent function to verify token scopes
    The ValidateContent function has been added to the authentication module
    to validate the scopes of a token. This function checks if a token has a
    specific scope and returns an error if the scope is not present or does
    not match the expected scope. This enhancement provides a more robust
    and secure way to handle token validation and ensures that only tokens
    with the correct scopes can access certain resources.

--- internal/authentication/verify.go
+++ internal/authentication/verify.go
@@ -19,6 +19,31 @@ var (
 	userReadRole        = "user-user"
 )
 
+func ValidateContent(
+	ctx context.Context,
+	scopeSlot string,
+	expectedScope string,
+) error {
+	token := middleware.TokenFromContext(ctx)
+	claims := token.PrivateClaims()
+	if _, ok := claims[scopeSlot]; !ok {
+		return fmt.Errorf(
+			"query without claim %s not allowed",
+			scopeSlot,
+		)
+	}
+	scopes := fmt.Sprintf("%v", claims[scopeSlot])
+	if !strings.Contains(scopes, expectedScope) {
+		return fmt.Errorf(
+			"given scope %s does not matches expected scope %s",
+			expectedScope,
+			scopes,
+		)
+	}
+
+	return nil
+}
+
 func CheckReadUser(ctx context.Context) error {
 	token := middleware.TokenFromContext(ctx)
 	claims := token.PrivateClaims()
