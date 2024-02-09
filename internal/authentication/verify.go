3868948c83f952b18f4504dbcd7a3b62b4682f2f
Siddhartha Basu <biosidd@gmail.com>
2024-02-09 12:57:39 -0600

    refactor(verify.go): remove unused content roles and scope checks
    The content roles and scope checks were removed from the verify.go file
    as they were not being used. This simplifies the code and makes it
    easier to maintain. The userReadRole was kept as it is still in use.

--- internal/authentication/verify.go
+++ internal/authentication/verify.go
@@ -10,13 +10,7 @@ import (
 )
 
 var (
-	// contentCreatorRole  = []string{"content-writer", "content-admin"}
-	// contentEditorRole   = []string{"content-editor", "content-admin"}
-	// contentDeleteRole   = []string{"content-admin", "content-remover"}
-	contentCreatorScope = "write:content"
-	contentEditorScope  = "edit:content"
-	contentDeleteScope  = "delete:content"
-	userReadRole        = "user-user"
+	userReadRole = "user-user"
 )
 
 func ValidateContent(
@@ -55,82 +49,6 @@ func CheckReadUser(ctx context.Context) error {
 	roles := fmt.Sprintf("%v", claims["roles"])
 	if !strings.Contains(roles, userReadRole) {
 		return errors.New("query without user-user roles not allowed")
-	}
-	return nil
-}
-
-func CheckCreateContent(ctx context.Context) error {
-	scopes, err := checkTokenClaims(ctx, "scope")
-	if err != nil {
-		return err
-	}
-	err = checkScope(scopes, contentCreatorScope)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func CheckDeleteContent(ctx context.Context) error {
-	scopes, err := checkTokenClaims(ctx, "scopes")
-	if err != nil {
-		return err
-	}
-	err = checkScope(scopes, contentDeleteScope)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func CheckUpdateContent(ctx context.Context) error {
-	scopes, err := checkTokenClaims(ctx, "scopes")
-	if err != nil {
-		return err
-	}
-	err = checkScope(scopes, contentEditorScope)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func checkTokenClaims(
-	ctx context.Context,
-	scopeSlot string,
-) (string, error) {
-	token := middleware.TokenFromContext(ctx)
-	claims := token.PrivateClaims()
-	if _, ok := claims[scopeSlot]; !ok {
-		return "", fmt.Errorf(
-			"query without claim %s not allowed",
-			scopeSlot,
-		)
-	}
-	scopes := fmt.Sprintf("%v", claims[scopeSlot])
-	return scopes, nil
-}
-
-/* func checkRole(roles string, expectedRoles []string) error {
-	rolesOk := false
-	for _, rls := range expectedRoles {
-		if strings.Contains(roles, rls) {
-			rolesOk = true
-			break
-		}
-	}
-	if !rolesOk {
-		return errors.New("query without proper role is not allowed")
-	}
-	return nil
-} */
-
-func checkScope(scopes string, expectedScope string) error {
-	if !strings.Contains(scopes, expectedScope) {
-		return errors.New("query without proper scope is not allowed")
 	}
 	return nil
 }
