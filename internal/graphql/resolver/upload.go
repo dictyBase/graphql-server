d2ac858c772da74f98133d2a3403c03c9bbc3150
Siddhartha Basu <biosidd@gmail.com>
2024-04-07 13:11:46 -0500

    feat(upload.go): add UploadFile function to handle file uploads
    The UploadFile function has been added to the resolver package. This
    function generates a unique ID for each file, uploads the file to an S3
    bucket, and returns a URL for the uploaded file. This feature enhances
    the application's functionality by allowing users to upload files. The
    function also handles errors that may occur during the file upload
    process, improving the robustness of the application.

---
+++ internal/graphql/resolver/upload.go
@@ -0,0 +1,50 @@
+package resolver
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/minio/minio-go/v7"
+
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
+	"github.com/dictyBase/graphql-server/internal/graphql/models"
+	"github.com/dictyBase/graphql-server/internal/registry"
+	"github.com/google/uuid"
+)
+
+func (mrs *MutationResolver) UploadFile(
+	ctx context.Context,
+	file graphql.Upload,
+) (*models.ImageFile, error) {
+	rndId, err := uuid.NewRandom()
+	if err != nil {
+		iderr := fmt.Errorf("error in generating random uuid %s", err)
+		errorutils.AddGQLError(ctx, iderr)
+		return nil, err
+	}
+	bucket := mrs.Registry.GetRecord(registry.S3Bucket)
+	fileInBucket := fmt.Sprintf(
+		"%s/%s/%s",
+		mrs.Registry.GetRecord(registry.S3BucketPath),
+		time.Now().Format(time.DateOnly),
+		rndId.String(),
+	)
+	uploadInfo, err := mrs.Registry.GetS3Client(registry.S3CLIENT).PutObject(
+		ctx, bucket, fileInBucket, file.File, file.Size, minio.PutObjectOptions{},
+	)
+	if err != nil {
+		uerr := fmt.Errorf("error in uploading file %s", file.Filename)
+		errorutils.AddGQLError(ctx, uerr)
+		return nil, uerr
+	}
+	mrs.Logger.Debug(uploadInfo)
+	return &models.ImageFile{
+		URL: fmt.Sprintf(
+			"%s/%s/%s",
+			mrs.Registry.GetAPIEndpoint(registry.S3STORAGE),
+			bucket,
+			fileInBucket,
+		)}, nil
+}
