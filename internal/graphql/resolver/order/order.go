c8ea1a84302743a1756b71b25cff3a3c3b532531
Siddhartha Basu <biosidd@gmail.com>
2024-04-30 16:58:25 -0500

    feat(order.go): add userByEmail function to retrieve user by email
    The changes were made to improve the readability of the code by breaking
    down long functions into smaller, more manageable ones. This makes the
    code easier to understand and maintain. The error handling was also
    improved to provide more detailed error messages. The userByEmail
    function was added to retrieve a user by their email, which is a common
    operation in this file.

--- internal/graphql/resolver/order/order.go
+++ internal/graphql/resolver/order/order.go
@@ -3,54 +3,91 @@ package order
 import (
 	"context"
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/dictyBase/graphql-server/internal/graphql/resolver/stock"
 
 	"github.com/dictyBase/aphgrpc"
-	"github.com/dictyBase/go-genproto/dictybaseapis/api/jsonapi"
 	pb "github.com/dictyBase/go-genproto/dictybaseapis/order"
 	stockPB "github.com/dictyBase/go-genproto/dictybaseapis/stock"
 	"github.com/dictyBase/go-genproto/dictybaseapis/user"
+	"github.com/dictyBase/graphql-server/internal/authentication"
 	"github.com/dictyBase/graphql-server/internal/graphql/errorutils"
 	"github.com/dictyBase/graphql-server/internal/graphql/models"
 	"github.com/sirupsen/logrus"
 )
 
+var numRgx = regexp.MustCompile("[0-9]+")
+
 type OrderResolver struct {
 	Client      pb.OrderServiceClient
 	StockClient stockPB.StockServiceClient
-	UserClient  user.UserServiceClient
+	UserClient  authentication.LogtoClient
 	Logger      *logrus.Entry
 }
 
-func (r *OrderResolver) ID(ctx context.Context, obj *pb.Order) (string, error) {
+func (ord *OrderResolver) ID(ctx context.Context, obj *pb.Order) (string, error) {
 	return obj.Data.Id, nil
 }
-func (r *OrderResolver) CreatedAt(ctx context.Context, obj *pb.Order) (*time.Time, error) {
+
+func (ord *OrderResolver) CreatedAt(
+	ctx context.Context,
+	obj *pb.Order,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.CreatedAt)
 	return &time, nil
 }
-func (r *OrderResolver) UpdatedAt(ctx context.Context, obj *pb.Order) (*time.Time, error) {
+
+func (ord *OrderResolver) UpdatedAt(
+	ctx context.Context,
+	obj *pb.Order,
+) (*time.Time, error) {
 	time := aphgrpc.ProtoTimeStamp(obj.Data.Attributes.UpdatedAt)
 	return &time, nil
 }
-func (r *OrderResolver) Courier(ctx context.Context, obj *pb.Order) (*string, error) {
+
+func (ord *OrderResolver) Courier(
+	ctx context.Context,
+	obj *pb.Order,
+) (*string, error) {
 	return &obj.Data.Attributes.Courier, nil
 }
-func (r *OrderResolver) CourierAccount(ctx context.Context, obj *pb.Order) (*string, error) {
+
+func (ord *OrderResolver) CourierAccount(
+	ctx context.Context,
+	obj *pb.Order,
+) (*string, error) {
 	return &obj.Data.Attributes.CourierAccount, nil
 }
-func (r *OrderResolver) Comments(ctx context.Context, obj *pb.Order) (*string, error) {
+
+func (ord *OrderResolver) Comments(
+	ctx context.Context,
+	obj *pb.Order,
+) (*string, error) {
 	return &obj.Data.Attributes.Comments, nil
 }
-func (r *OrderResolver) Payment(ctx context.Context, obj *pb.Order) (*string, error) {
+
+func (ord *OrderResolver) Payment(
+	ctx context.Context,
+	obj *pb.Order,
+) (*string, error) {
 	return &obj.Data.Attributes.Payment, nil
 }
-func (r *OrderResolver) PurchaseOrderNum(ctx context.Context, obj *pb.Order) (*string, error) {
+
+func (ord *OrderResolver) PurchaseOrderNum(
+	ctx context.Context,
+	obj *pb.Order,
+) (*string, error) {
 	return &obj.Data.Attributes.PurchaseOrderNum, nil
 }
-func (r *OrderResolver) Status(ctx context.Context, obj *pb.Order) (*models.StatusEnum, error) {
+
+func (ord *OrderResolver) Status(
+	ctx context.Context,
+	obj *pb.Order,
+) (*models.StatusEnum, error) {
 	status := obj.Data.Attributes.Status
 	switch status {
 	case pb.OrderStatus_IN_PREPARATION:
@@ -69,63 +106,132 @@ func (r *OrderResolver) Status(ctx context.Context, obj *pb.Order) (*models.Stat
 		return nil, fmt.Errorf("incompatible order status")
 	}
 }
-func (r *OrderResolver) Consumer(ctx context.Context, obj *pb.Order) (*user.User, error) {
-	user := user.User{}
+
+func (ord *OrderResolver) Consumer(
+	ctx context.Context,
+	obj *pb.Order,
+) (*user.User, error) {
 	email := obj.Data.Attributes.Consumer
-	g, err := r.UserClient.GetUserByEmail(ctx, &jsonapi.GetEmailRequest{Email: email})
+	g, err := ord.userByEmail(ctx, email)
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
-		r.Logger.Error(err)
-		return &user, err
+		ord.Logger.Error(err)
+		return nil, err
 	}
-	r.Logger.Debugf("successfully found user with email %s", email)
+	ord.Logger.Debugf("successfully found user with email %s", email)
 	return g, nil
 }
-func (r *OrderResolver) Payer(ctx context.Context, obj *pb.Order) (*user.User, error) {
-	user := user.User{}
+
+func (ord *OrderResolver) Payer(
+	ctx context.Context,
+	obj *pb.Order,
+) (*user.User, error) {
 	email := obj.Data.Attributes.Payer
-	g, err := r.UserClient.GetUserByEmail(ctx, &jsonapi.GetEmailRequest{Email: email})
+	g, err := ord.userByEmail(ctx, email)
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
-		r.Logger.Error(err)
-		return &user, err
+		ord.Logger.Error(err)
+		return nil, err
 	}
-	r.Logger.Debugf("successfully found user with email %s", email)
+	ord.Logger.Debugf("successfully found user with email %s", email)
 	return g, nil
 }
-func (r *OrderResolver) Purchaser(ctx context.Context, obj *pb.Order) (*user.User, error) {
-	user := user.User{}
+
+func (ord *OrderResolver) Purchaser(
+	ctx context.Context,
+	obj *pb.Order,
+) (*user.User, error) {
 	email := obj.Data.Attributes.Purchaser
-	g, err := r.UserClient.GetUserByEmail(ctx, &jsonapi.GetEmailRequest{Email: email})
+	g, err := ord.userByEmail(ctx, email)
 	if err != nil {
 		errorutils.AddGQLError(ctx, err)
-		r.Logger.Error(err)
-		return &user, err
+		ord.Logger.Error(err)
+		return nil, err
 	}
-	r.Logger.Debugf("successfully found user with email %s", email)
+	ord.Logger.Debugf("successfully found user with email %s", email)
 	return g, nil
 }
-func (r *OrderResolver) Items(ctx context.Context, obj *pb.Order) ([]models.Stock, error) {
+
+func (ord *OrderResolver) Items(
+	ctx context.Context,
+	obj *pb.Order,
+) ([]models.Stock, error) {
 	stocks := []models.Stock{}
 	for _, id := range obj.Data.Attributes.Items {
 		if id[:3] == "DBS" {
-			gs, err := r.StockClient.GetStrain(ctx, &stockPB.StockId{Id: id})
+			gs, err := ord.StockClient.GetStrain(ctx, &stockPB.StockId{Id: id})
 			if err != nil {
 				errorutils.AddGQLError(ctx, err)
-				r.Logger.Error(err)
+				ord.Logger.Error(err)
 				return stocks, err
 			}
-			stocks = append(stocks, stock.ConvertToStrainModel(id, gs.Data.Attributes))
+			stocks = append(
+				stocks,
+				stock.ConvertToStrainModel(id, gs.Data.Attributes),
+			)
 		}
 		if id[:3] == "DBP" {
-			gp, err := r.StockClient.GetPlasmid(ctx, &stockPB.StockId{Id: id})
+			gp, err := ord.StockClient.GetPlasmid(ctx, &stockPB.StockId{Id: id})
 			if err != nil {
 				errorutils.AddGQLError(ctx, err)
-				r.Logger.Error(err)
+				ord.Logger.Error(err)
 				return stocks, err
 			}
-			stocks = append(stocks, stock.ConvertToPlasmidModel(id, gp.Data.Attributes))
+			stocks = append(
+				stocks,
+				stock.ConvertToPlasmidModel(id, gp.Data.Attributes),
+			)
 		}
 	}
 	return stocks, nil
+}
+
+func (ord *OrderResolver) userByEmail(
+	ctx context.Context,
+	email string,
+) (*user.User, error) {
+	userResp, err := ord.UserClient.UserWithEmail(email)
+	if err != nil {
+		userErr := fmt.Errorf("unable to retreieve user %s", err)
+		errorutils.AddGQLError(ctx, userErr)
+		ord.Logger.Error(userErr)
+		return nil, userErr
+	}
+	matches := numRgx.FindAllString(userResp.ID, -1)
+	if len(matches) == 0 {
+		nonumErr := fmt.Errorf(
+			"cannot convert user id to number %s",
+			userResp.ID,
+		)
+		errorutils.AddGQLError(ctx, nonumErr)
+		ord.Logger.Error(nonumErr)
+		return nil, nonumErr
+	}
+	userID, err := strconv.ParseInt(strings.Join(matches, ""), 10, 64)
+	if err != nil {
+		parseErr := fmt.Errorf("unable to convert user id to integer %s", err)
+		errorutils.AddGQLError(ctx, parseErr)
+		ord.Logger.Error(parseErr)
+		return nil, parseErr
+	}
+	ord.Logger.Debugf("successfully found user with id %s", email)
+	return &user.User{
+		Data: &user.UserData{
+			Type: "user",
+			Id:   userID,
+			Attributes: &user.UserAttributes{
+				FirstName:    userResp.Username,
+				LastName:     userResp.Name,
+				Email:        userResp.PrimaryEmail,
+				Organization: userResp.CustomData.Institution,
+				FirstAddress: userResp.CustomData.Address,
+				City:         userResp.CustomData.City,
+				State:        userResp.CustomData.State,
+				Zipcode:      userResp.CustomData.Zipcode,
+				Country:      userResp.CustomData.Country,
+				Phone:        userResp.PrimaryPhone,
+				IsActive:     true,
+			},
+		},
+	}, nil
 }
