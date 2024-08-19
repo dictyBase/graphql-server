7f24b0e12c0d60982e6673d50635e2dc4efa0a62
Siddhartha Basu <siddhartha-basu@northwestern.edu>
2024-08-19 15:30:15 -0500

    feat(download.generated.go): add marshaling functions for Organism model

--- internal/graphql/generated/download.generated.go
+++ internal/graphql/generated/download.generated.go
@@ -942,4 +942,68 @@ func (ec *executionContext) marshalNDownloadItem2ᚖgithubᚗcomᚋdictyBaseᚋg
 	return ec._DownloadItem(ctx, sel, v)
 }
 
+func (ec *executionContext) marshalNOrganism2ᚖgithubᚗcomᚋdictyBaseᚋgraphqlᚑserverᚋinternalᚋgraphqlᚋmodelsᚐOrganism(ctx context.Context, sel ast.SelectionSet, v *models.Organism) graphql.Marshaler {
+	if v == nil {
+		if !graphql.HasFieldError(ctx, graphql.GetFieldContext(ctx)) {
+			ec.Errorf(ctx, "the requested element is null which the schema does not allow")
+		}
+		return graphql.Null
+	}
+	return ec._Organism(ctx, sel, v)
+}
+
+func (ec *executionContext) marshalOOrganism2ᚕᚖgithubᚗcomᚋdictyBaseᚋgraphqlᚑserverᚋinternalᚋgraphqlᚋmodelsᚐOrganismᚄ(ctx context.Context, sel ast.SelectionSet, v []*models.Organism) graphql.Marshaler {
+	if v == nil {
+		return graphql.Null
+	}
+	ret := make(graphql.Array, len(v))
+	var wg sync.WaitGroup
+	isLen1 := len(v) == 1
+	if !isLen1 {
+		wg.Add(len(v))
+	}
+	for i := range v {
+		i := i
+		fc := &graphql.FieldContext{
+			Index:  &i,
+			Result: &v[i],
+		}
+		ctx := graphql.WithFieldContext(ctx, fc)
+		f := func(i int) {
+			defer func() {
+				if r := recover(); r != nil {
+					ec.Error(ctx, ec.Recover(ctx, r))
+					ret = nil
+				}
+			}()
+			if !isLen1 {
+				defer wg.Done()
+			}
+			ret[i] = ec.marshalNOrganism2ᚖgithubᚗcomᚋdictyBaseᚋgraphqlᚑserverᚋinternalᚋgraphqlᚋmodelsᚐOrganism(ctx, sel, v[i])
+		}
+		if isLen1 {
+			f(i)
+		} else {
+			go f(i)
+		}
+
+	}
+	wg.Wait()
+
+	for _, e := range ret {
+		if e == graphql.Null {
+			return graphql.Null
+		}
+	}
+
+	return ret
+}
+
+func (ec *executionContext) marshalOOrganism2ᚖgithubᚗcomᚋdictyBaseᚋgraphqlᚑserverᚋinternalᚋgraphqlᚋmodelsᚐOrganism(ctx context.Context, sel ast.SelectionSet, v *models.Organism) graphql.Marshaler {
+	if v == nil {
+		return graphql.Null
+	}
+	return ec._Organism(ctx, sel, v)
+}
+
 // endregion ***************************** type.gotpl *****************************
